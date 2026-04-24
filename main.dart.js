639e0cc84201bd1586f397bfb5065908360d6128
LEO0331 <LEO0331@users.noreply.github.com>
2026-04-24 00:51:31 +0000

    deploy: 215c8625884876489189e49ef707ce7a773d2093

--- main.dart.js
+++ main.dart.js
@@ -34760,7 +34760,7 @@ $S:2}
 A.aUk.prototype={
 $1(a){var s=A.e9().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/425cfb54d01a9472b3e81d9e76fd63a4a44cfbcb/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/59aa584fdf100e6c78c785d8a5b565d1de4b48ab/":s)+a},
 $S:33}
 A.WG.prototype={
 gA(a){var s=this.a
